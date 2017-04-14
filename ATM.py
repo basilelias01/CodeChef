8b1fae4f5127ad79b0a5bdb98fa350c23dcc6032
Basil <basilelias01@gmail.com>
2017-04-14 20:54:35 +0530

    First Commit

---
+++ ATM.py
@@ -0,0 +1,9 @@
+
+
+val, bal = raw_input().split()
+val, bal = int(val),float(bal)
+if (0 < val <= 2000)and (0 < bal <= 20000):
+    if (val % 5 == 0) and (bal > (val + 0.5)):
+        print (" %.2f " % (bal-val-0.5))
+    else:
+        print (" %.2f " % bal)
